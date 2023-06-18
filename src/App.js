f1d48440d056fedfe187dd9338cec6501142796f
Codeforyou404 <codeforyou404@gmail.com>
2023-06-18 18:33:13 +0530

    folders

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React,{Provider} from 'react';
 import Home from './pages/home/Home';
-import Header from './component/Header/Header';
+import Header from './component/header/Header';
 import SideBar from './component/sidebar/SideBar';
 
 function App() {
